0d9ee790defb7590aaa52077e315b230c7674a55
Ioana Enache <ioaanaa.en@gmail.com>
2021-03-30 19:49:46 +0300

    curs05

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends React.Component
+  constructor () {
+    super();
+    this.state = {}
+  }
+
+
+  render() {
+    return (
+      <div>
+        Curs 05
+      </div>
+    )
+  }
+
+
+
+
+
+
+
+export default App;
